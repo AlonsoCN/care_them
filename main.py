97b8f3b075262f8e796fc1beaa4331c3bd3382a7
Alonso Calle <luis.alonso.cn@gmail.com>
2017-04-22 17:21:36 -0500

    main.py for Flask

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
-import click
+from flask import Flask, render_template
 
-@click.group()
-def start_application():
-    """ CARE APP: Application to detect twits with emotional problems"""
-    pass
+app = Flask(__name__)
 
-@click.command()
-@click.option('--path', default=None, help="YAML Configuration path file.")
-def load_configuration(path):
-    if path is None:
-        click.echo('You must to define a correct path file. (i.e. --path)')
-    else:
-        click.echo('loading :)')
+@app.route("/")
+def index():
+    """ Serve client-side application """
+    return render_template('index.html')
 
-start_application.add_command(load_configuration)
-
-if __name__ == '__main__':
-    start_application()
+if __name__ == "__main__":
+    app.run(debug=True)
